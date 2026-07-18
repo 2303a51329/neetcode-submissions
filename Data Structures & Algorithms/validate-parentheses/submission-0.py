58f43bb3dd279b4ab27c5a6a6760643facebae17
Kashaboina Archana <kashaboinaarchana168@gmail.com>
2026-07-18 21:48:37 +0530

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def isValid(self, s):
+        stack = []
+        for ch in s:
+            if ch == "(":
+                stack.append(")")
+            elif ch == "[":
+                stack.append("]")
+            elif ch == "{":
+                stack.append("}")
+            else:
+                if not stack or stack[-1] != ch:
+                    return False
+                stack.pop()
+        return not stack
